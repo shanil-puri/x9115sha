6c5c1a0501aea34a8c44ca7728c6530878d17d9e
shanilp <spuri3@ncsu.edu>
2015-09-11 09:07:50 +0000

    Uncommented all test cases

--- hw/code/2/think3.py
+++ hw/code/2/think3.py
@@ -1,84 +1,84 @@
-# def repeat_lyrics():
-#     print_lyrics()
-#     print_lyrics()
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
 
-# def print_lyrics():
-#     print "I'm a lumberjack, and I'm okay."
-#     print "I sleep all night and I work all day."
+def print_lyrics():
+    print "I'm a lumberjack, and I'm okay."
+    print "I sleep all night and I work all day."
 
-# repeat_lyrics()
+repeat_lyrics()
 
 # Excercise 3.3
-# def right_justify(s):
-#     print " " * (70 - len(s)) + s
+def right_justify(s):
+    print " " * (70 - len(s)) + s
     
-# right_justify("allen")
+right_justify("allen")
 
 #Excercise 3.4.1
-# def do_twice(f):
-#     f()
-#     f()
+def do_twice(f):
+    f()
+    f()
     
-# def print_spam():
-#     print "spam"
+def print_spam():
+    print "spam"
     
-# do_twice(print_spam)
+do_twice(print_spam)
 
 #Excercise 3.4.2
-# def do_twice(f, v):
-#     f(v)
-#     f(v)
+def do_twice(f, v):
+    f(v)
+    f(v)
     
-# def print_spam(s):
-#     print s
+def print_spam(s):
+    print s
     
-# do_twice(print_spam, "rand")
+do_twice(print_spam, "rand")
 
 #Excercise 3.4.3
-# def do_twice(f, v):
-#     f(v)
-#     f(v)
+def do_twice(f, v):
+    f(v)
+    f(v)
     
-# def print_twice(s):
-#     print s
-#     print s
+def print_twice(s):
+    print s
+    print s
     
-# do_twice(print_twice, "rand")
+do_twice(print_twice, "rand")
 
-# #Excercise 3.4.4
-# def do_twice(f, v):
-#     f(v)
-#     f(v)
+#Excercise 3.4.4
+def do_twice(f, v):
+    f(v)
+    f(v)
     
-# def print_twice(s):
-#     print s
-#     print s
+def print_twice(s):
+    print s
+    print s
     
-# do_twice(print_twice, "spam")
+do_twice(print_twice, "spam")
 
 #Excercise 3.4.5
-# def do_twice(f, v):
-#     f(v)
-#     f(v)
+def do_twice(f, v):
+    f(v)
+    f(v)
     
-# def print_twice(s):
-#     print s
-#     print s
+def print_twice(s):
+    print s
+    print s
     
-# def do_four(f, v):
-#     do_twice(f, v)
-#     do_twice(f, v)
+def do_four(f, v):
+    do_twice(f, v)
+    do_twice(f, v)
     
-# do_four(print_twice, "3.4.5")
+do_four(print_twice, "3.4.5")
 
 # #Excercise 3.5.1
-# def draw_grid():
-#     for i in range(0, 11):
-#         if(i%5 == 0):
-#             print "+", (" _"*4 + "+")*2
-#         else:
-#             print "|", ("  "*4 + "|")*2
-# draw_grid()
+def draw_grid():
+    for i in range(0, 11):
+        if(i%5 == 0):
+            print "+", (" _"*4 + "+")*2
+        else:
+            print "|", ("  "*4 + "|")*2
+draw_grid()
 
 #Excercise 3.5.2
 def draw_row(num_cols):
